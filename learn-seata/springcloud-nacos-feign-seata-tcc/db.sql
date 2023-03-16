74a77549500aa184d7cf9d7050de8790407eb18d
liwenhui <38020818@qq.com>
2023-03-17 00:09:50 +0800

    修改TCC数据库

--- learn-seata/springcloud-nacos-feign-seata-tcc/db.sql
+++ learn-seata/springcloud-nacos-feign-seata-tcc/db.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE `seata_tcc_storage` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE seata_tcc_storage;
+
 DROP TABLE IF EXISTS `stock_tbl`;
 CREATE TABLE `stock_tbl`
 (
@@ -8,6 +11,23 @@ CREATE TABLE `stock_tbl`
     PRIMARY KEY (`id`),
     UNIQUE KEY (`commodity_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `tcc_fence_log`
+(
+    `xid`           VARCHAR(128)  NOT NULL COMMENT 'global id',
+    `branch_id`     BIGINT        NOT NULL COMMENT 'branch id',
+    `action_name`   VARCHAR(64)   NOT NULL COMMENT 'action name',
+    `status`        TINYINT       NOT NULL COMMENT 'status(tried:1;committed:2;rollbacked:3;suspended:4)',
+    `gmt_create`    DATETIME(3)   NOT NULL COMMENT 'create time',
+    `gmt_modified`  DATETIME(3)   NOT NULL COMMENT 'update time',
+    PRIMARY KEY (`xid`, `branch_id`),
+    KEY `idx_gmt_modified` (`gmt_modified`),
+    KEY `idx_status` (`status`)
+    ) ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4;
+
+CREATE DATABASE `seata_tcc_order` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE seata_tcc_order;
 
 DROP TABLE IF EXISTS `order_tbl`;
 CREATE TABLE `order_tbl`
@@ -21,6 +41,23 @@ CREATE TABLE `order_tbl`
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `tcc_fence_log`
+(
+    `xid`           VARCHAR(128)  NOT NULL COMMENT 'global id',
+    `branch_id`     BIGINT        NOT NULL COMMENT 'branch id',
+    `action_name`   VARCHAR(64)   NOT NULL COMMENT 'action name',
+    `status`        TINYINT       NOT NULL COMMENT 'status(tried:1;committed:2;rollbacked:3;suspended:4)',
+    `gmt_create`    DATETIME(3)   NOT NULL COMMENT 'create time',
+    `gmt_modified`  DATETIME(3)   NOT NULL COMMENT 'update time',
+    PRIMARY KEY (`xid`, `branch_id`),
+    KEY `idx_gmt_modified` (`gmt_modified`),
+    KEY `idx_status` (`status`)
+    ) ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4;
+
+CREATE DATABASE `seata_tcc_account` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE seata_tcc_account;
+
 DROP TABLE IF EXISTS `account_tbl`;
 CREATE TABLE `account_tbl`
 (
@@ -30,22 +67,6 @@ CREATE TABLE `account_tbl`
     `freeze_money`  int(11) DEFAULT 0,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 CREATE TABLE IF NOT EXISTS `tcc_fence_log`
